74daa38eefd4bc0fb24700e845ebe17b6262ff3d
Sauyon Lee <git@sauyon.com>
2013-11-25 21:20:04 -0800

    compile js using closure on post-receive

---
+++ jscompile.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -jar $1 --js /srv/chat/js/vendor/jquery.js --js /srv/chat/js/vendor/jquery.md5.js --js /srv/chat/js/vendor/bigint.js --js /srv/chat/js/vendor/crypto.js --js /srv/chat/js/vendor/eventemmiter.js --js /srv/chat/js/vendor/otr.js --js /srv/chat/js/client.js --js /srv/chat/js/vendor/custom.modernizr.js --js_output_file /srv/chat/js/moechat.min.js --create_source_map /srv/chat/js/moechat.min.map --warning_level QUIET
