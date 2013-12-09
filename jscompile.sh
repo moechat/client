17800e8043e5d404b8462283e40a8e90768127ff
Sauyon Lee <git@sauyon.com>
2013-12-08 22:36:49 -0800

    clean up jscompile.sh

--- jscompile.sh
+++ jscompile.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
 cd /srv/chat/js
-java -jar $1 --js vendor/jquery.js --js vendor/foundation.min.js --js vendor/jquery.md5.js --js vendor/bigint.js --js vendor/crypto.js --js vendor/eventemitter.js --js vendor/otr.js --js imgupload.js --js options.js --js client.js --js vendor/modernizr.js --js_output_file moechat.min.js --create_source_map moechat.min.map --warning_level QUIET
+java -jar $1 \
+	--js vendor/jquery.js \
+	--js vendor/foundation.min.js \
+	--js vendor/jquery.md5.js \
+	--js vendor/bigint.js \
+	--js vendor/crypto.js \
+	--js vendor/eventemitter.js \
+	--js vendor/otr.js \
+	--js imgupload.js \
+	--js options.js \
+	--js client.js \
+	--js vendor/modernizr.js \
+	--create_source_map ./moechat.min.map \
+	--source_map_format V3 \
+	--warning_level QUIET \
+	--js_output_file moechat.min.js
